d6b7c73142e74a589d66be8b12ab1ad9cafd5726
Ray <raymond.sandi@outlook.com>
2024-06-18 17:00:15 -0600

    paginas añadidas

--- Website_Menu_Project/ProyectoMenu/urls.py
+++ Website_Menu_Project/ProyectoMenu/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.inicio, name='inicio'),
+    path('productos/', views.productos, name='productos'),
+    path('contacto/', views.contacto, name='contacto'),
+    path('servicios/', views.servicios, name='servicios')
 ]
--- Website_Menu_Project/ProyectoMenu/views.py
+++ Website_Menu_Project/ProyectoMenu/views.py
@@ -6,3 +6,14 @@ from django.shortcuts import render
 def inicio(request):
     return render(request, 'paginas/inicio.html')
     return render(request, 'base.html')
+
+def productos(request):
+    return render(request, 'paginas/productos.html')
+    return render(request, 'base.html')
+    
+def servicios(request):
+    return render(request, 'paginas/servicios.html')
+    return render(request, 'base.html')
+def contacto(request):
+    return render(request, 'paginas/contacto.html')
+    return render(request, 'base.html')
